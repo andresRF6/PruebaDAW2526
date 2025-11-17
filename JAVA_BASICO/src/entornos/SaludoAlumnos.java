98182dd636788da69ab5183b5bc62976e578812d
achillon <andres.chillonsesma@educa.madrid.org>
2025-11-17 13:40:19 +0100

    Version1 Ejercicio Saludos

---
+++ JAVA_BASICO/src/entornos/SaludoAlumnos.java
@@ -0,0 +1,10 @@
+
+package entornos;
+
+public class SaludoAlumnos {
+
+    public static void main(String[] args) {
+        System.out.println("Buenos días, os quiero saludar a TOD@S!!");
+        System.out.println("Devolvedme el saludo con nombre y Apellidos. Buena práctica a valorar de cara a la nota final ;)");
+    }
+}
